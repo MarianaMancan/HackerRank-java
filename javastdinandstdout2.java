3c1882ffe813b815822f5ec5f914bd952e5f892f
Mariana Mancan <marianamancan@gmail.com>
2024-08-01 11:55:22 -0300

    fazendo desafios de java

--- javastdinandstdout2.java
+++ javastdinandstdout2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class javastdinandstdout2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);

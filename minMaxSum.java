69b44613f92afaa5374e10d14be2ae522dce91d6
Mariana Mancan <marianamancan@gmail.com>
2024-08-01 23:12:08 -0300

    treinando

--- minMaxSum.java
+++ minMaxSum.java
@@ -7,38 +7,44 @@ import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
 import java.util.stream.*;
-
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class minMaxSum {
+class miniMaxSum {
 
-   
 
-   public static void main(String[] args) throws IOException {
-    
-     Scanner scan = new Scanner(System.in);
-    
-      int[] numbers = new int[5];
-        for (int i = 0; i < 5; i++) {
-            numbers[i] = scan.nextInt();
-        }
-        scan.close();
+ public static void miniMaxSum(List<Integer> arr) {
+       
+        Collections.sort(arr);
         
-        Arrays.sort(numbers);
-        
-        Long minSum = 0L;
-        Long maxSum = 0L;
+     
+        long minSum = 0;
         for (int i = 0; i < 4; i++) {
-            minSum += numbers[i];
+            minSum += arr.get(i);
         }
-       for (int i = 1; i < 5; i++) {
-           maxSum += numbers[i];
-       }
 
-       System.out.println(minSum + " " + maxSum);
+       
+        long maxSum = 0;
+        for (int i = 1; i < 5; i++) {
+            maxSum += arr.get(i);
+        }
 
-
+      
+        System.out.println(minSum + " " + maxSum);
     }
 
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        miniMaxSum.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
 }
